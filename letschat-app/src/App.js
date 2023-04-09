27151a1bef5672cad1685d6077683a479467d2d3
ak478gh <amitkumar655921@gmail.com>
2023-04-09 08:38:22 +0530

    Component folder added with Navbar in it

--- letschat-app/src/App.js
+++ letschat-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Img } from '@chakra-ui/react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import PicAndText from './Components/PicAndText/PicAndText.jsx';
 
+let props = {
+  hd1: 'alkjsdf'
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className='banner'>
+        <Img src='https://scontent.whatsapp.net/v/t39.8562-34/316546300_547692113846445_7299710494491288098_n.png?ccb=1-7&_nc_sid=2fbf2a&_nc_ohc=IF7bBz_Ob6oAX-etos1&_nc_ht=scontent.whatsapp.net&oh=01_AdSfTgVoRWXVSzT1NlTEOExlyGy4IFRDyaqdu6KfvCKEQQ&oe=64370065' width="95vw" borderRadius='15px'/>
+        <PicAndText/>
+      </div>
     </div>
   );
 }
